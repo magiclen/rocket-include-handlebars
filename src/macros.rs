4a546093e2085af348488b6b0aa654bd293b96e6
Magic Len <len@magiclen.org>
2020-02-26 19:55:01 +0800

    fix "drop a reference"

--- src/macros.rs
+++ src/macros.rs
@@ -84,8 +84,10 @@ macro_rules! handlebars_response {
 #[cfg(debug_assertions)]
 macro_rules! handlebars_response_cache {
     ($cm:expr, $key:expr, $gen:block) => {{
-        drop($cm);
-        drop($key);
+        #[allow(unused_variables)]
+        let __a = &$cm;
+        #[allow(unused_variables)]
+        let __a = &$key;
         $gen
     }};
 }
