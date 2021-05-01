cb9398dda0cf4b746778f90c8076f8f55c16da58
Magic Len <len@magiclen.org>
2021-05-01 08:26:41 +0800

    update doc

--- src/lib.rs
+++ src/lib.rs
@@ -3,7 +3,7 @@
 
 This is a crate which provides macros `handlebars_resources_initialize!` and `handlebars_response!` to statically include HBS (Handlebars) files from your Rust project and make them be the HTTP response sources quickly.
 
-* `handlebars_resources_initialize!` is used in the fairing of `HandlebarsResponse` to include Handlebars files into your executable binary file. You need to specify each file's name and its path. In order to reduce the compilation time and allow to hot-reload templates, files are compiled into your executable binary file together, only when you are using the **release** profile.
+* `handlebars_resources_initialize!` is used in the fairing of `HandlebarsResponse` to include Handlebars files into your executable binary file. You need to specify each file's name and its path relative to the directory containing the manifest of your package. In order to reduce the compilation time and allow to hot-reload templates, files are compiled into your executable binary file together, only when you are using the **release** profile.
 * `handlebars_response!` is used for retrieving and rendering the file you input through the macro `handlebars_resources_initialize!` as a `HandlebarsResponse` instance with rendered HTML. When its `respond_to` method is called, three HTTP headers, **Content-Type**, **Content-Length** and **Etag**, will be automatically added, and the rendered HTML can optionally not be minified.
 * `handlebars_response_cache!` is used for wrapping a `HandlebarsResponse` and its constructor, and use a **key** to cache its HTML and ETag in memory. The cache is generated only when you are using the **release** profile.
 * `handlebars_resources_initializer!` is used for generating a fairing for handlebars resources.
@@ -20,15 +20,15 @@ extern crate handlebars;
 #[macro_use]
 extern crate educe;
 
-#[doc(hidden)]
-pub extern crate slash_formatter;
+
 
 mod functions;
 
 #[cfg(debug_assertions)]
 mod debug;
 
-#[cfg(not(debug_assertions))]
+#[cfg(not(debug_assertions))]#[doc(hidden)]
+pub extern crate slash_formatter;
 mod release;
 
 mod macros;
