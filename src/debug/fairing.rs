a2e6e62bbb3a2947084de515c89350e191895783
Magic Len <len@magiclen.org>
2021-06-13 20:53:34 +0800

    fix Rocket

--- src/debug/fairing.rs
+++ src/debug/fairing.rs
@@ -38,7 +38,7 @@ impl Fairing for HandlebarsResponseFairing {
     }
 
     #[inline]
-    async fn on_request(&self, req: &mut Request<'_>, _data: &mut Data) {
+    async fn on_request(&self, req: &mut Request<'_>, _data: &mut Data<'_>) {
         let cm = req
             .rocket()
             .state::<HandlebarsContextManager>()
